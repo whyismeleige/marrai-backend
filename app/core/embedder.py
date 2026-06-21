f0a51380b77edc9fbfa14a124f3be853618e3870
whyismeleige <pjain.work@proton.me>
2026-06-21 22:36:53 +0530

    feat(embedder): add ChunkEmbedding class and refactor return for the new class

--- app/core/embedder.py
+++ app/core/embedder.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import numpy
 from sentence_transformers import SentenceTransformer
 
@@ -5,6 +7,13 @@ from app.core.chunker import TextChunk
 
 PASSAGE_PREFIX = ""
 QUERY_PREFIX = "Represent this sentence for searching relevant passages: "
+
+
+@dataclass
+class ChunkEmbedding:
+    heading_embedding: numpy.ndarray
+    content_embedding: numpy.ndarray
+
 
 _model = None
 
@@ -18,18 +27,32 @@ def _get_model():
     return _model
 
 
-def embed(chunks: list[TextChunk]) -> list[tuple[TextChunk, numpy.ndarray]]:
+def embed(chunks: list[TextChunk]) -> list[tuple[TextChunk, ChunkEmbedding]]:
     model = _get_model()
 
-    texts_to_encode = [
-        f"{PASSAGE_PREFIX}{chunk.heading}\n{chunk.content}" for chunk in chunks
-    ]
+    heading_texts_to_encode = [chunk.heading for chunk in chunks]
 
-    embeddings = model.encode(
-        texts_to_encode,
+    content_texts_to_encode = [chunk.content for chunk in chunks]
+
+    heading_embeddings = model.encode(
+        heading_texts_to_encode,
         batch_size=32,
         normalize_embeddings=True,
         convert_to_numpy=True,
     )
+
+    content_embeddings = model.encode(
+        content_texts_to_encode,
+        batch_size=32,
+        normalize_embeddings=True,
+        convert_to_numpy=True,
+    )
+
+    embeddings = [
+        ChunkEmbedding(heading_embedding, content_embedding)
+        for heading_embedding, content_embedding in zip(
+            heading_embeddings, content_embeddings
+        )
+    ]
 
     return list(zip(chunks, embeddings))
